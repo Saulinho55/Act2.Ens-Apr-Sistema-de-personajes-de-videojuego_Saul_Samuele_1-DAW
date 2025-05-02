2f4bdc3bb6ad99274aaaafc6996cc8e5a1435007
Saulinho55 <Saulespino555@gmail.com>
2025-05-02 16:58:23 +0100

    Update Arquero.java

--- src/sistemapersonajes/clases/Arquero.java
+++ src/sistemapersonajes/clases/Arquero.java
@@ -1,16 +1,15 @@
 package sistemapersonajes.clases;
 import sistemapersonajes.interfaces.Movilizable;
 import sistemapersonajes.interfaces.Volador;
-import sistemapersonajes.subclases.PersonajeFisico; // Ensure this class exists in the specified package
+import sistemapersonajes.subclases.PersonajeFisico;
 
 public class Arquero extends PersonajeFisico implements Volador, Movilizable{
     private int agilidad;
-    private int numeFlechas;
-
-    public Arquero(String nombre, int nivel, int salud, int agilidad, int numeFlechas) {
+    private int numFlechas;
+    public Arquero(String nombre, int nivel, int salud, int agilidad, int numFlechas) {
         super(nombre, nivel, salud);
         this.agilidad = agilidad;
-        this.numeFlechas = numeFlechas;
+        this.numFlechas = numFlechas;
     }
     @Override
     public void atacar() {
@@ -23,5 +22,14 @@ public class Arquero extends PersonajeFisico implements Volador, Movilizable{
 
     public void reabastecerFlechas() {
         System.out.println("El arquero " + nombre + " reabastece sus flechas.");
+    }
+    @Override
+    public void moverse() {
+        System.out.println("El arquero " + nombre + " se mueve.");
+    }
+
+    @Override
+    public void volar() {
+        System.out.println("El arquero " + nombre + " vuela.");
     }
 }
